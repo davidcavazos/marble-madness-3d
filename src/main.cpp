31583e4056debe5704a850c23b2ef023ef0d74c4
Mono <davido262@gmail.com>
2012-09-20 17:53:07 -0500

    moved engine files to engine/ folder

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,14 @@
 
 #include <iostream>
 #include <cstdlib>
-#include "device/devicemanager.hpp"
-#include "terminal/terminal.hpp"
+#include "game.hpp"
 
 using namespace std;
 
 int main(int, char**) {
-    Device* device = DeviceManager::createSystem(DEVICE_SDL_OPENGL_LEGACY);
-    device->setTitle("Marble Madness 3D");
-    device->setResolution(800, 500);
-
-    bool isRunning = true;
-    while (isRunning) {
-        device->processEvents(isRunning);
-    }
-
-    DeviceManager::shutdownSystem();
-
+    Game game("game");
+    game.loadScene();
+    game.bindControls();
+    game.runGameLoop();
     return EXIT_SUCCESS;
 }
